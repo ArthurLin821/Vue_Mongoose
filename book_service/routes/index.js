fc278fe490e381d6f4d9f2ea1666fc30c6889f1f
林万彦 <flittermouse@msn.com>
2019-01-29 22:11:59 +0800

    建立中间件Mongoose以连接MongonDB，新建路由测试连接是否成功

--- book_service/routes/index.js
+++ book_service/routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
 
+// express示例
+var express = require('express');
+// 路由引入
+var router = express.Router();
+// 数据库引入
+var mongoose = require('mongoose');
+
+
+// 定义路由
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+// 定义路由
+router.checkout('mongooseTest', function (req, res, next) {
+  mongoose.connect('mongodb://localhost/pets', { useMongoClient: true });
+  mongoose.Promise = global.Promise;
+
+  var Cat = mongoose.model('Cat', { name: string });
+
+  var tom = new Cat({ name: 'Tom ' });
+  tom.save(function (err) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log('success inserted')
+    }
+
+  })
+
+})
+
 
 module.exports = router;
